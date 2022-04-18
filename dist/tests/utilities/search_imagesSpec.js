f8ce7cb4c451b994867f4bad4b94719ec77974dc
MohamedDiaaEldin <mdiaan404@gmail.com>
2022-04-18 03:04:55 +0200

    update readme file

--- dist/tests/utilities/search_imagesSpec.js
+++ dist/tests/utilities/search_imagesSpec.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var search_images_1 = require("../../utilities/search_images");
 describe('test search image function ', function () {
     var image_name = "fjord";
-    it('test with valid image', function (done) {
+    it('test with valid image name', function (done) {
         (0, search_images_1.is_image_there)(image_name).then(function (result) {
             expect(result).toBeTruthy();
         }).catch(function (result) {
@@ -11,7 +11,7 @@ describe('test search image function ', function () {
         });
         done();
     });
-    it('test with not valid valid image', function (done) {
+    it('test with not valid valid image name', function (done) {
         (0, search_images_1.is_image_there)(image_name).then(function (result) {
             // 
         }).catch(function (result) {
